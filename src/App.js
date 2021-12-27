390259e05ffeb2561397daa59ccde86c5249d789
Mohsin Javaid <muhsanjavaid@gmail.com>
2021-12-27 09:57:14 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import TodoForm from './componentes/TodoForm';
+
+function App() {
+  return (
+    <div className="todo-app">
+     <TodoForm/>
+    </div>
+  );
+}
+
+export default App;
